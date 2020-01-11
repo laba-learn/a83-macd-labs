0828e3eb1843b3f564c69dcfca9a7195c0fc2a08
Dreamcheck <main.dreamcheck@gmail.com>
2020-01-11 18:39:32 +0800

    add database profile

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "labaappserver.database.windows.net";
+    $user = "labasubagia22";
+    $pass = "NasiGoreng99";
+    $db = "labadb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
